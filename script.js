f57dc78ba6b3268bd97f059696a52d88a41b3e12
Camel <camel@Camel.localdomain>
2020-03-13 03:20:42 +0100

    mobile version done

---
+++ script.js
@@ -0,0 +1,16 @@
+// 'use strict'
+// const mobile = window.matchMedia('(max-width: 480px)');
+// const menu = document.querySelector('.nav-container');
+
+
+// function toggleMenuClass(){
+//     if(mobile.matches){
+//         menu.classList.add('mobile-menu');
+//     }
+//     else{
+//         menu.classList.remove('mobile-media');
+//     }
+// }
+
+
+// mobile.addEventListener('change', toggleMenuClass)
