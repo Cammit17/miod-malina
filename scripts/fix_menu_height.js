7ffe9e44fc63c371bd7c6b7a734a1dba2790f0fc
Camel <camel@Camel.localdomain>
2020-03-17 16:45:03 +0100

    final commit v2

--- scripts/fix_menu_height.js
+++ scripts/fix_menu_height.js
@@ -1,11 +1,25 @@
 let windowY = window.screen.height;
 const body = document.querySelector('body')
-const menu = body.querySelector('.menu')
+const navCnt = document.querySelector('.nav-container');
+const toggler = navCnt.querySelector('.toggler')
+const menu = navCnt.querySelector('.menu')
+const media = window.matchMedia('(max-device-width: 1000px)')
 
 function fixMenuBgHeight(){
     menu.style.height = `${body.scrollHeight}px`;
 }
+if(media.matches){
+    navCnt.classList.add('mobile-menu');
+    fixMenuBgHeight()
+}
+else{
+    navCnt.classList.remove('mobile-menu');
+    navCnt.classList.add('desktop-menu')
+    toggler.style.display = 'none';
+}
 
-fixMenuBgHeight()
+
+
+
 
 console.log(body.scrollHeight)
